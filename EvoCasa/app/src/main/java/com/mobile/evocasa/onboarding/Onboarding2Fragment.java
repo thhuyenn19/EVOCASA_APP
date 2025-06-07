12b4ffdef49bc531aa9244d1f196a1c987f4c532
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-06-07 20:01:49 +0700

    initial commit

--- EvoCasa/app/src/main/java/com/mobile/evocasa/onboarding/Onboarding2Fragment.java
+++ EvoCasa/app/src/main/java/com/mobile/evocasa/onboarding/Onboarding2Fragment.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.VideoView;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
 
 import com.mobile.evocasa.R;
 
@@ -23,9 +28,7 @@ import java.io.File;
  */
 public class Onboarding2Fragment extends Fragment {
 
-    private VideoView videoBackground;
-    private MediaPlayer mMediaPlayer;
-    private int mCurrentVideoPosition = 0;
+    VideoView videoView;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -71,75 +74,38 @@ public class Onboarding2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_onboarding2, container, false);
-
         View view = inflater.inflate(R.layout.fragment_onboarding2, container, false);
 
-        // Hook up the VideoView to our UI.
-        videoBackground = view.findViewById(R.id.videoBackground);
+        videoView = view.findViewById(R.id.videoview);
+        Uri uri = Uri.parse("android.resource://" + requireContext().getPackageName() + "/" + R.raw.vid_onboarding2);
+        videoView.setVideoURI(uri);
+        videoView.start();
 
-        // Build your video Uri
-        Uri uri = Uri.parse("android.resource://"
-                + requireContext().getPackageName()
-                + "/"
-                + R.raw.vid_onboarding2);
-
-        // Set the new Uri to our VideoView
-        videoBackground.setVideoURI(uri);
-
-        // Set OnPreparedListener for looping and resume behavior
-        videoBackground.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mMediaPlayer = mediaPlayer;
-                mMediaPlayer.setLooping(true);
-                mMediaPlayer.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
-
-                if (mCurrentVideoPosition != 0) {
-                    mMediaPlayer.seekTo(mCurrentVideoPosition);
-                }
-                videoBackground.start();
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
             }
-
         });
 
         return view;
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        if (videoBackground != null) {
-            mCurrentVideoPosition = videoBackground.getCurrentPosition();
-            videoBackground.pause();
-        }
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
-        // Bật full screen immersive mode
-        requireActivity().getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
-
-        if (videoBackground != null) {
-            videoBackground.seekTo(mCurrentVideoPosition);
-            videoBackground.start();
-        }
+        videoView.resume();
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-        }
+    public void onPause() {
+        videoView.suspend();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        videoView.stopPlayback();
+        super.onDestroy();
     }
 }
