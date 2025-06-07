6cebd65ce6d2b9d403c68c66c321ab83e532faa3
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-06-07 14:33:22 +0700

    initial commit

--- EvoCasa/app/build.gradle.kts
+++ EvoCasa/app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("me.relex:circleindicator:2.1.6")
 }
