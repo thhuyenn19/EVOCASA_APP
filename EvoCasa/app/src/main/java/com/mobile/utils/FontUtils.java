17e9efd5a2cfdf0f0d32fbbe4eddc27d9e5466b2
Thanh Huyen <thhuyenn19@gmail.com>
2025-06-07 22:39:36 +0700

    Update FontUtils.java

--- EvoCasa/app/src/main/java/com/mobile/utils/FontUtils.java
+++ EvoCasa/app/src/main/java/com/mobile/utils/FontUtils.java
@@ -2,6 +2,7 @@ package com.mobile.utils;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.widget.TextView;
 
 public class FontUtils {
     private static Typeface bold;
@@ -11,6 +12,13 @@ public class FontUtils {
     private static Typeface italic;
     private static Typeface regular;
     private static Typeface mediumitalic;
+
+    private static Typeface Zblack;
+    private static Typeface Zbold;
+    private static Typeface Zmedium;
+    private static Typeface Zregular;
+    private static Typeface Zsemibold;
+
     private static boolean initialized = false;
 
     public static void initFonts(Context context) {
@@ -22,6 +30,13 @@ public class FontUtils {
             italic = Typeface.createFromAsset(context.getAssets(), "fonts/Inter-Italic.otf");
             regular = Typeface.createFromAsset(context.getAssets(), "fonts/Inter-Regular.otf");
             mediumitalic = Typeface.createFromAsset(context.getAssets(), "fonts/Inter-MediumItalic.otf");
+
+            Zblack = Typeface.createFromAsset(context.getAssets(), "fonts/ZenOldMincho-Black.ttf");
+            Zbold = Typeface.createFromAsset(context.getAssets(), "fonts/ZenOldMincho-Bold.ttf");
+            Zmedium = Typeface.createFromAsset(context.getAssets(), "fonts/ZenOldMincho-Medium.ttf");
+            Zregular = Typeface.createFromAsset(context.getAssets(), "fonts/ZenOldMincho-Regular.ttf");
+            Zsemibold = Typeface.createFromAsset(context.getAssets(), "fonts/ZenOldMincho-SemiBold.ttf");
+
             initialized = true;
         }
     }
@@ -55,36 +70,82 @@ public class FontUtils {
         if (!initialized) initFonts(context);
         return regular;
     }
+
     public static Typeface getMediumitalic(Context context) {
         if (!initialized) initFonts(context);
         return mediumitalic;
     }
 
+    public static Typeface getZblack(Context context) {
+        if (!initialized) initFonts(context);
+        return Zblack;
+    }
 
-    public static void setBoldFont(Context context, android.widget.TextView textView) {
+    public static Typeface getZbold(Context context) {
+        if (!initialized) initFonts(context);
+        return Zbold;
+    }
+
+    public static Typeface getZmedium(Context context) {
+        if (!initialized) initFonts(context);
+        return Zmedium;
+    }
+
+    public static Typeface getZregular(Context context) {
+        if (!initialized) initFonts(context);
+        return Zregular;
+    }
+
+    public static Typeface getZsemibold(Context context) {
+        if (!initialized) initFonts(context);
+        return Zsemibold;
+    }
+
+    public static void setBoldFont(Context context, TextView textView) {
         textView.setTypeface(getBold(context));
     }
 
-    public static void setMediumFont(Context context, android.widget.TextView textView) {
+    public static void setMediumFont(Context context, TextView textView) {
         textView.setTypeface(getMedium(context));
     }
 
-    public static void setSemiBoldFont(Context context, android.widget.TextView textView) {
+    public static void setSemiBoldFont(Context context, TextView textView) {
         textView.setTypeface(getSemiBold(context));
     }
 
-    public static void setBlackFont(Context context, android.widget.TextView textView) {
+    public static void setBlackFont(Context context, TextView textView) {
         textView.setTypeface(getBlack(context));
     }
 
-    public static void setItalicFont(Context context, android.widget.TextView textView) {
+    public static void setItalicFont(Context context, TextView textView) {
         textView.setTypeface(getItalic(context));
     }
 
-    public static void setRegularFont(Context context, android.widget.TextView textView) {
+    public static void setRegularFont(Context context, TextView textView) {
         textView.setTypeface(getRegular(context));
     }
-    public static void setMediumitalic(Context context, android.widget.TextView textView) {
+
+    public static void setMediumitalicFont(Context context, TextView textView) {
         textView.setTypeface(getMediumitalic(context));
+    }
+
+    public static void setZblackFont(Context context, TextView textView) {
+        textView.setTypeface(getZblack(context));
+    }
+
+    public static void setZboldFont(Context context, TextView textView) {
+        textView.setTypeface(getZbold(context));
+    }
+
+    public static void setZmediumFont(Context context, TextView textView) {
+        textView.setTypeface(getZmedium(context));
+    }
+
+    public static void setZregularFont(Context context, TextView textView) {
+        textView.setTypeface(getZregular(context));
+    }
+
+    public static void setZsemiboldFont(Context context, TextView textView) {
+        textView.setTypeface(getZsemibold(context));
     }
 }
