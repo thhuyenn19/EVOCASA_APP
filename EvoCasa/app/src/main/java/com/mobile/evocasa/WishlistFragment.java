d2b0a3c6fef2060e3ec704ae6202e09a97bddb32
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-06-08 13:38:25 +0700

    initial commit

--- EvoCasa/app/src/main/java/com/mobile/evocasa/WishlistFragment.java
+++ EvoCasa/app/src/main/java/com/mobile/evocasa/WishlistFragment.java
@@ -3,7 +3,6 @@ package com.mobile.evocasa;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,11 +11,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.mobile.models.WishlistProduct;
-import com.mobile.models.WishlistRcm;
 import com.mobile.utils.FontUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
