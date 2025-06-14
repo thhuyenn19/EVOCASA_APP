69573437c53efb76bd33cd4b9f3a81e60540a02d
ThuyTrang1604 <trangptt22411c@st.uel.edu.vn>
2025-06-14 15:35:12 +0700

    blog and noti edit commit

--- EvoCasa/app/src/main/java/com/mobile/evocasa/NotificationFragment.java
+++ EvoCasa/app/src/main/java/com/mobile/evocasa/NotificationFragment.java
@@ -39,19 +39,20 @@ public class NotificationFragment extends Fragment {
 
         // Tạo danh sách NotificationItem
         List<NotificationItem> items = new ArrayList<>();
+        items.add(new NotificationItem("TODAY"));
         items.add(new NotificationItem(R.drawable.ic_order_delivered, "Order Delivered",
-                "Order 250421T3W123 is completed. Your feedback matters to others! Rate the products by 26-05-2025 and get up 200 coins.", "11:10", false));
+                "Order 250421T3W123 is completed. Your feedback matters to others! Rate the products by 26-05-2025 and get up 200 coins.", " Today 11:10", false));
         items.add(new NotificationItem(R.drawable.ic_complete_payment, "Complete Your Payment",
-                "Your order of 239.000 VND has not been paid. Please complete your payment by  20-05-2025 18:50. Ignore this message if you've already paid.", "08:50", true));
+                "Your order of 239.000 VND has not been paid. Please complete your payment by  20-05-2025 18:50. Ignore this message if you've already paid.", "Today 08:50", true));
         items.add(new NotificationItem(R.drawable.ic_payment_confirmed, "Payment Confirmed",
-                "Payment for order 250417T4W506 has been confirmed. Kindly wait for your shipment.", "08:50", true));
+                "Payment for order 250417T4W506 has been confirmed. Kindly wait for your shipment and be ready to receive order in the next 3 days.", " Today 07:30", true));
         items.add(new NotificationItem("YESTERDAY"));
         items.add(new NotificationItem(R.drawable.ic_order_cancelled, "Order Cancelled",
-                "Order 250416T7W124 has been cancelled by Evocasa System. Apologies for any inconvenience caused.", "07:30", false));
+                "Order 250416T7W124 has been cancelled by Evocasa System. Apologies for any inconvenience caused.", "Yesterday 22:05", false));
         items.add(new NotificationItem(R.drawable.ic_order_delivered, "Order  Delivered",
-                "Order 250410T9W302 is completed. Your feedback matters to others! Rate the products by 25-05-2025 and get up 200 coins.", "07:30", false));
+                "Order 250410T9W302 is completed. Your feedback matters to others! Rate the products by 25-05-2025 and get up 200 coins.", "Yesterday 21:45", false));
         items.add(new NotificationItem(R.drawable.ic_complete_payment, "Complete Your Payment",
-                "Your order of 850.000 VND has not been paid. Please complete your payment by  20-05-2025 13:50. Ignore this message if you've already paid.", "11:10", true));
+                "Your order of 850.000 VND has not been paid. Please complete your payment by  20-05-2025 13:50. Ignore this message if you've already paid.", "Yesterday 07:30", true));
         items.add(new NotificationItem(R.drawable.ic_order_delivered, "Order Delivered",
                 "Order 250421T3W123 is completed. Your feedback matters!", "11:10", true));
 
@@ -69,10 +70,6 @@ public class NotificationFragment extends Fragment {
     }
 
     private void applyCustomFonts(View view) {
-        TextView txtNotificationDate = view.findViewById(R.id.txtNotificationDate);
-        if (txtNotificationDate != null) {
-            FontUtils.setRegularFont(getContext(), txtNotificationDate);
-        }
 
         TextView txtMarkAllRead = view.findViewById(R.id.txtMarkAllRead);
         if (txtMarkAllRead != null) {
