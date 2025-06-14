f721b3f64636b9949fc657c5df4458b9c223b8e4
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-06-14 15:15:08 +0700

    initial commit

--- EvoCasa/app/src/main/java/com/mobile/evocasa/LeaveReviewActivity.java
+++ EvoCasa/app/src/main/java/com/mobile/evocasa/LeaveReviewActivity.java
@@ -33,7 +33,6 @@ public class LeaveReviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_leave_review);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
