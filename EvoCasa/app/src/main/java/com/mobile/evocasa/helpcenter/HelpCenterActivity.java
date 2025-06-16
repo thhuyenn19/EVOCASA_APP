c85647c8090f47830afc99b91667c8f3f32f9172
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-06-16 17:35:37 +0700

    intial commit

---
+++ EvoCasa/app/src/main/java/com/mobile/evocasa/helpcenter/HelpCenterActivity.java
@@ -0,0 +1,26 @@
+package com.mobile.evocasa.helpcenter;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mobile.evocasa.R;
+
+public class HelpCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_help_center);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
