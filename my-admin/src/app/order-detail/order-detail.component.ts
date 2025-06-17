df7ea1a954e4d865a2542ad8ef35bd08a2f63f6a
Nhung Pham Tuyet <131536332+nhungnehehe@users.noreply.github.com>
2025-06-17 11:17:19 +0700

    initial commit

--- my-admin/src/app/order-detail/order-detail.component.ts
+++ my-admin/src/app/order-detail/order-detail.component.ts
@@ -125,8 +125,6 @@ export class OrderDetailComponent implements OnInit {
       day: '2-digit',
       month: '2-digit',
       year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
     });
   }
 
