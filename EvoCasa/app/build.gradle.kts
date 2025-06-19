c5f595fbe82836bce54bb5522dc1c04a4360b2c2
Thanh Huyen <thhuyenn19@gmail.com>
2025-06-19 23:08:53 +0700

    commit

--- EvoCasa/app/build.gradle.kts
+++ EvoCasa/app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation ("com.facebook.android:facebook-android-sdk:latest.release")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 
 
 
