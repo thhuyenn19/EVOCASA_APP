eb7092c6e9a9dfd5214bd26d45ff65f436139962
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-06-29 21:40:16 +0700

    initial commit

--- EvoCasaAdmin/app/src/main/java/com/thanhhuyen/evocasaadmin/CustomerDetailActivity.java
+++ EvoCasaAdmin/app/src/main/java/com/thanhhuyen/evocasaadmin/CustomerDetailActivity.java
@@ -1,6 +1,9 @@
 package com.thanhhuyen.evocasaadmin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.thanhhuyen.untils.FontUtils;
+
 public class CustomerDetailActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        initViews();
+    }
+
+    private void initViews() {
+        imgBack = findViewById(R.id.imgBack);
+        if (imgBack != null) {
+            imgBack.setOnClickListener(v -> {
+                Intent intent = new Intent(CustomerDetailActivity.this, CustomerManagementActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                finish();
+            });
+        }
+
+        // Font setup
+        txtTitle = findViewById(R.id.txtTitle);
+        if (txtTitle != null) {
+            FontUtils.setZboldFont(this, txtTitle);
+        }
     }
 }
