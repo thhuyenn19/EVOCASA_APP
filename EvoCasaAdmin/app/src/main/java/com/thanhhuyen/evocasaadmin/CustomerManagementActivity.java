1f5ebfc21171d652771d7f255d11c701a8f34383
Lê Nguyễn Minh Thảo <thaolnm22411c@st.uel.edu.vn>
2025-06-29 15:59:37 +0700

    initial commit

--- EvoCasaAdmin/app/src/main/java/com/thanhhuyen/evocasaadmin/CustomerManagementActivity.java
+++ EvoCasaAdmin/app/src/main/java/com/thanhhuyen/evocasaadmin/CustomerManagementActivity.java
@@ -2,6 +2,8 @@ package com.thanhhuyen.evocasaadmin;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,8 +19,12 @@ import com.thanhhuyen.untils.FontUtils;
 public class CustomerManagementActivity extends AppCompatActivity {
 
     private RecyclerView customerRecyclerView;
-    private TextView txtTitle, txtSearchHint;
+    private TextView txtTitle, txtTotalCustomersValue,
+            txtTotalCustomersTitle, txtTotalCustomersPercent, txtNewCustomersValue,
+            txtNewCustomersTitle, txtNewCustomersPercent, txtTotalOrdersValue,
+            txtTotalOrdersTitle, txtTotalOrdersPercent;
     private ImageView imgBack;
+    private EditText edtSearch;
 
 
     @Override
@@ -42,7 +48,60 @@ public class CustomerManagementActivity extends AppCompatActivity {
         // Font setup
         txtTitle = findViewById(R.id.txtTitle);
         if (txtTitle != null) {
-            FontUtils.setBoldFont(this, txtTitle);
+            FontUtils.setZboldFont(this, txtTitle);
         }
+        edtSearch = findViewById(R.id.edtSearch);
+         if (edtSearch != null) {
+             FontUtils.setRegularFont(this, edtSearch);
+         }edtSearch = findViewById(R.id.edtSearch);
+        if (edtSearch != null) {
+            FontUtils.setRegularFont(this, edtSearch);
+        }
+
+        TextView txtTotalCustomersValue = findViewById(R.id.txtTotalCustomersValue);
+        if (txtTotalCustomersValue != null) {
+            FontUtils.setBoldFont(this, txtTotalCustomersValue);
+        }
+
+        TextView txtTotalCustomersTitle = findViewById(R.id.txtTotalCustomersTitle);
+        if (txtTotalCustomersTitle != null) {
+            FontUtils.setRegularFont(this, txtTotalCustomersTitle);
+        }
+
+        TextView txtTotalCustomersPercent = findViewById(R.id.txtTotalCustomersPercent);
+        if (txtTotalCustomersPercent != null) {
+            FontUtils.setBoldFont(this, txtTotalCustomersPercent);
+        }
+
+        TextView txtNewCustomersValue = findViewById(R.id.txtNewCustomersValue);
+        if (txtNewCustomersValue != null) {
+            FontUtils.setBoldFont(this, txtNewCustomersValue);
+        }
+
+        TextView txtNewCustomersTitle = findViewById(R.id.txtNewCustomersTitle);
+        if (txtNewCustomersTitle != null) {
+            FontUtils.setRegularFont(this, txtNewCustomersTitle);
+        }
+
+        TextView txtNewCustomersPercent = findViewById(R.id.txtNewCustomersPercent);
+        if (txtNewCustomersPercent != null) {
+            FontUtils.setBoldFont(this, txtNewCustomersPercent);
+        }
+
+//        TextView txtTotalOrdersValue = findViewById(R.id.txtTotalOrdersValue);
+//        if (txtTotalOrdersValue != null) {
+//            FontUtils.setBoldFont(this, txtTotalOrdersValue);
+//        }
+//
+//        TextView txtTotalOrdersTitle = findViewById(R.id.txtTotalOrdersTitle);
+//        if (txtTotalOrdersTitle != null) {
+//            FontUtils.setRegularFont(this, txtTotalOrdersTitle);
+//        }
+//
+//        TextView txtTotalOrdersPercent = findViewById(R.id.txtTotalOrdersPercent);
+//        if (txtTotalOrdersPercent != null) {
+//            FontUtils.setBoldFont(this, txtTotalOrdersPercent);
+//        }
+
     }
 }
